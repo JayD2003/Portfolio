cc7c9a42d527b3eb338af4771da291c9ff0b9566
JayD2003 <jayasaidodla@gmail.com>
2025-04-06 11:58:27 +0530

    updated routes and animations

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // this line ensures direct access works
+  },
 })
